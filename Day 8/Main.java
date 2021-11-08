2137d8e3355c6cf28d6061b51a9dda1b70a2f454
shubham23278 <stongaria45@gmail.com>
2021-11-08 20:45:48 +0530

    Day 8

---
+++ Day 8/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int d = getValueIndecimal(n, b);
+      System.out.println(d);
+   }
+  
+   public static int getValueIndecimal(int n, int b){
+
+    int new_value = 0;
+    int power = 0;
+
+    while(n > 0){
+        int digit = n % 10;
+        new_value += digit * Math.pow(b , power);
+        power++; 
+        n /= 10;
+    }
+    return new_value;
+   }
+  }
