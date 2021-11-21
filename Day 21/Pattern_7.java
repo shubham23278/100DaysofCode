87d204c9359cfa6d524418b74e0c7441ea3dad53
shubham23278 <stongaria45@gmail.com>
2021-11-21 23:37:11 +0530

    Day 21

---
+++ Day 21/Pattern_7.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Pattern_7 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == j) {
+                    System.out.print("*\t");
+                    break;
+                }else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
